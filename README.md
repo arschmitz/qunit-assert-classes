44a02ed7ac7992d106c56caefc53b0df7cc8b9a5
Alexander Schmitz <arschmitz@gmail.com>
2015-03-11 22:44:34 -0400

    Update to add new regex, strict, start, and partial matches
    Also addes tests to make sure the asserts fail when they should

--- README.md
+++ README.md
@@ -12,28 +12,151 @@ A set of assertions for checking thats an element has ( or does not have ) any n
 ```js
 assert.hasClasses( element, classes [, message] );
 assert.lacksClasses( element, classes [, message] );
+
+assert.hasClassesStrict( element, classes [, message] );
+
+assert.hasClassRegex( element, RexExp [, message] );
+assert.lacksClassRegex( element, RexExp [, message] );
+
+assert.hasClassStart( element, start [, message] );
+assert.lacksClassStart( element, start [, message] );
+
+assert.hasClassPartial( element, partial [, message] );
+assert.lacksClassPartial( element, partial [, message] );
+
 ```
 
 Where:
  - `element`: a jQuery object or DOM Element ( must be a single element, collections not allowed )
  - `classes`: a space seperated string of classes
  - `message`: a custom message to replace default message of "Element must (not) have classes"
+ - `RegExp`: a valid regular expression to be used to match against individual class names
+ - `start`: a string that should match the begining of any class
+ - `partial`: a string of a partial class name that should be matched against any of the classes
 
 ## Examples
 
 ### Example 1: hasClasses with DOM Element
+```html
+<div class="class1 class2"></div>
+```
+
 ```js
 QUnit.test('Example hasClasses unit test', function( assert ) {
   var element = document.getElementById( "test" );
-  assert.hasClasses( element, "class1 class2" );
-  assert.hasClasses( element, "class1 class2", "custom message" );
+
+  assert.hasClasses( element, "class1 class2" ); // Passes
+  assert.hasClasses( element, "class3", "custom message" ); // Fails
 });
 ```
 
 ### Example 2: lacksClasses with jQuery object
+```html
+<div class="class1 class2"></div>
+```
+
 ```js
 QUnit.test('Example lacksClasses unit test', function( assert ) {
   var element = $( "#test" );
-  assert.lacksClasses( element, "missing-class" );
-  assert.lacksClasses( element, "missing-class missing-class-2", "custom message" );
+
+  assert.lacksClasses( element, "missing-class" ); // Passes
+  assert.lacksClasses( element, "class1 missing-class-2", "custom message" ); // Fails
 });
+
+### Example 3: hasClassesStrict
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example hasClassesStrict unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.hasClassesStrict( element, "class1 class2" ); // Passes
+  assert.hasClassesStrict( element, "class1", "custom message" ); // Fails
+});
+```
+
+### Example 4: hasClassRegex
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example hasClassesRegex unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.hasClassRegex( element, /^(clas)([a-z]){1}([0-9])$/ ); // Passes
+
+  // Fails this only matches against individual classes
+  assert.hasClassRegex( element, /^(([a-z]){5}([0-9]|[0-9]\s)){2}$/, "custom message" );
+});
+```
+
+### Example 5: lacksClassRegex
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example hasClassesRegex unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.lacksClassRegex( element, /boom/ ); // Passes
+  assert.lacksClassRegex( element, /^(class)/, "custom message" ); // Fails
+});
+```
+
+### Example 6: hasClassStart
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example hasClassStart unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.hasClassStart( element, "cla" ); // Passes
+  assert.hasClassStart( element, "lass", "custom message" ); // Fails
+});
+```
+### Example 7: lacksClassStart
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example lacksClassStart unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.lacksClassStart( element, "foo" ); // Passes
+  assert.lacksClassStart( element, "cla", "custom message" ); // Fails
+});
+```
+### Example 8: hasClassPartial
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example hasClassPartial unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.hasClassPartial( element, "lass" ); // Passes
+  assert.hasClassPartial( element, "foo", "custom message" ); // Fails
+});
+```
+### Example 7: lacksClassPartial
+```html
+<div class="class1 class2"></div>
+```
+
+```js
+QUnit.test('Example lacksClassStart unit test', function( assert ) {
+  var element = document.getElementById( "test" );
+
+  assert.lacksClassPartial( element, "foo" ); // Passes
+  assert.lacksClassPartial( element, "lass", "custom message" ); // Fails
+});
+```
+
--- tests/classes-test.js
+++ tests/classes-test.js
@@ -1,7 +1,7 @@
 QUnit.module( "qunit-assert-classes plugin unit tests" );
 
 QUnit.test( "hasClasses", function( assert ) {
-	QUnit.expect( 20 );
+	QUnit.expect( 10 );
 	var element = $( "#test" );
 	assert.hasClasses( element, "one" );
 	assert.hasClasses( element, "two" );
@@ -13,6 +13,11 @@ QUnit.test( "hasClasses", function( assert ) {
 	assert.hasClasses( element, "extra two" );
 	assert.hasClasses( element, "one three" );
 	assert.hasClasses( element, "extra two three" );
+});
+
+QUnit.test( "hasClasses - custom message", function( assert ) {
+	QUnit.expect( 10 );
+	var element = $( "#test" );
 	assert.hasClasses( element, "one", "custom message" );
 	assert.hasClasses( element, "two", "custom message" );
 	assert.hasClasses( element, "three", "custom message" );
@@ -25,6 +30,7 @@ QUnit.test( "hasClasses", function( assert ) {
 	assert.hasClasses( element, "extra two three", "custom message" );
 });
 
+
 QUnit.test( "lacksClasses", function( assert ) {
 	QUnit.expect( 6 );
 	var element = $( "#test" );
@@ -35,6 +41,62 @@ QUnit.test( "lacksClasses", function( assert ) {
 	assert.lacksClasses( element, "one-x two-x", "custom message" );
 	assert.lacksClasses( element, "partial", "does not allow partial matches" );
 	assert.lacksClasses( element, "test", "missing class attribute does not throw error" );
+});
+
+QUnit.test( "hasClassesStrict", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassesStrict( element, "one two three extra partial-match" );
+	assert.hasClassesStrict( element, "one two three extra partial-match", "custom message" );
+});
+
+QUnit.test( "hasClassRegex", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassRegex( element, /^par([a-z]){4}-(match)$/ );
+	assert.hasClassRegex( element, /^par([a-z]){4}-(match)$/, "custom message" );
+});
+
+QUnit.test( "lacksClassRegex", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.lacksClassRegex( element, /^par([a-z]){4}-(mabtch)$/ );
+	assert.lacksClassRegex( element, /^par([a-z]){4}-(mabtch)$/, "custom message" );
+});
+
+QUnit.test( "hasClassStart", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassStart( element, "partial-" );
+	assert.hasClassStart( element, "partial-", "custom message" );
+});
+
+QUnit.test( "lacksClassStart", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.lacksClassStart( element, "partiald-" );
+	assert.lacksClassStart( element, "partiald-", "custom message" );
+});
+
+QUnit.test( "hasClassPartial", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassPartial( element, "ial-" );
+	assert.hasClassPartial( element, "ial-", "custom message" );
+});
+
+QUnit.test( "lacksClassPartial", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.lacksClassPartial( element, "iald-" );
+	assert.lacksClassPartial( element, "iald-", "custom message" );
 });
 
 QUnit.test( "hasClasses - dom node", function( assert ) {
@@ -73,4 +135,146 @@ QUnit.test( "lacksClasses - dom node", function( assert ) {
 	assert.lacksClasses( element, "one-x two-x", "custom message" );
 	assert.lacksClasses( element, "partial", "does not allow partial matches" );
 	assert.lacksClasses( element, "test", "missing class attribute does not throw error" );
+});
+
+QUnit.test( "hasClassesStrict - dom node", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.hasClassesStrict( element, "one two three extra partial-match" );
+	assert.hasClassesStrict( element, "one two three extra partial-match", "custom message" );
+});
+
+QUnit.test( "hasClassRegex - dom node", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.hasClassRegex( element, /^par([a-z]){4}-(match)$/ );
+	assert.hasClassRegex( element, /^par([a-z]){4}-(match)$/, "custom message" );
+});
+
+QUnit.test( "lacksClassRegex - dom node", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.lacksClassRegex( element, /^par([a-z]){4}-(mabtch)$/ );
+	assert.lacksClassRegex( element, /^par([a-z]){4}-(mabtch)$/, "custom message" );
+});
+
+QUnit.test( "hasClassStart", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.hasClassStart( element, "partial-" );
+	assert.hasClassStart( element, "partial-", "custom message" );
+});
+
+QUnit.test( "lacksClassStart", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.lacksClassStart( element, "partiald-" );
+	assert.lacksClassStart( element, "partiald-", "custom message" );
+});
+
+QUnit.test( "hasClassPartial", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.hasClassPartial( element, "ial-" );
+	assert.hasClassPartial( element, "ial-", "custom message" );
+});
+
+QUnit.test( "lacksClassPartial", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.lacksClassPartial( element, "iald-" );
+	assert.lacksClassPartial( element, "iald-", "custom message" );
+});
+
+QUnit.module( "Qunit-assert-classes ensure failures", {
+	beforeEach: function( assert ) {
+		var originalPush = assert.push;
+
+		assert.push = function( result, actual, expected, message ) {
+
+			// inverts the result so we can test failing assertions
+			originalPush( !result, actual, expected, message );
+		};
+	}
+});
+
+QUnit.test( "hasClasses - failures", function( assert ) {
+	QUnit.expect( 2 );
+
+	var element = $( "#test" );
+
+	assert.hasClasses( element, "missing-class", "hasClasses fails when non existent class is passed" );
+	assert.hasClasses( element, "one missing-class", "hasClasses fails when mix of existing and missing classes passed" );
+});
+
+QUnit.test( "lacksClasses - failures", function( assert ) {
+	QUnit.expect( 2 );
+
+	var element = $( "#test" );
+
+	assert.lacksClasses( element, "one", "lacksClasses fails when non existen class is passed" );
+	assert.lacksClasses( element, "one missing-class", "lacksClasses fails whencmix of existing and missing classes passed" );
+});
+
+QUnit.test( "hasClassesStrict - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassesStrict( element, "one" );
+	assert.hasClassesStrict( element, "one two", "custom message" );
+});
+
+QUnit.test( "hasClassRegex - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassRegex( element, /^par([a-z]){4}-(mabtch)$/ );
+	assert.hasClassRegex( element, /^par([a-z]){4}-(mabtch)$/, "custom message" );
+});
+
+QUnit.test( "hasClassRegex - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.lacksClassRegex( element, /^par([a-z]){4}-(match)$/ );
+	assert.lacksClassRegex( element, /^par([a-z]){4}-(match)$/, "custom message" );
+});
+
+QUnit.test( "hasClassStart - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.hasClassStart( element, "partiald-" );
+	assert.hasClassStart( element, "partiald-", "custom message" );
+});
+
+QUnit.test( "lacksClassStart - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" )[ 0 ];
+
+	assert.lacksClassStart( element, "partial-" );
+	assert.lacksClassStart( element, "partial-", "custom message" );
+});
+
+QUnit.test( "hasClassPartial - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.hasClassPartial( element, "iald-" );
+	assert.hasClassPartial( element, "iald-", "custom message" );
+});
+
+QUnit.test( "lacksClassPartial - failures", function( assert ) {
+	QUnit.expect( 2 );
+	var element = $( "#test" );
+
+	assert.lacksClassPartial( element, "ial-" );
+	assert.lacksClassPartial( element, "ial-", "custom message" );
 });
